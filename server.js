b872dad90f700f4b8b5f5c1a55c797997ee7ca0c
Ravneet Kaur <ravneet.kaur9629@gmail.com>
2023-10-31 20:37:49 -0700

    added viewallroles function

--- server.js
+++ server.js
@@ -14,45 +14,68 @@ const app = async () => {
 };
 
 const main = async () => {
-  await inquirer.prompt([
-    {
+  await inquirer
+    .prompt([
+      {
         type: "list",
         name: "main-menu",
         message: "Select from the options below.",
         choices: [
-            "view all departments", 
-            "view all roles",
-            "view all employees", 
-            "add a department", 
-            "add a role", 
-            "add an employee", 
-            "update an employee role",
-            "exit app"
-        ]
-    },
-
-]).then(async (answer) => {
-    if (answer.menu === "view all departments") {
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+          "exit app",
+        ],
+      },
+    ])
+    .then(async (answer) => {
+      if (answer.menu === "view all departments") {
         viewAllDepartments();
-    } else if (answer.menu === "view all roles"){
+      } else if (answer.menu === "view all roles") {
         viewAllRoles();
-    }else if (answer.menu === "view all employees"){
+      } else if (answer.menu === "view all employees") {
         viewAllEmployees();
-    }else if (answer.menu === "add a department"){
+      } else if (answer.menu === "add a department") {
         addDepartment();
-    }
-    else if (answer.menu === "add a role"){
+      } else if (answer.menu === "add a role") {
         AddRole();
-    }
-    else if (answer.menu === "add an employee"){
+      } else if (answer.menu === "add an employee") {
         AddEmployee();
-    }   
-    else if (answer.menu === "update an employee role"){
+      } else if (answer.menu === "update an employee role") {
         UpdateEmployee();
-    }   
-    else if (answer.menu === "exit app"){
-      console.log("goodbye!");
-    connection.end();
-    }   
-});
+      } else if (answer.menu === "exit app") {
+        console.log("goodbye!");
+        connection.end();
+      }
+    });
 };
+
+const viewAllDepartments = async () => {
+  connection.query("SELECT * FROM department", async (error, response) => {
+    if (error) {
+      console.log("An Error Exists!");
+      console.log(error.message);
+    }
+    console.log(table.print(response));
+    await main();
+  });
+};
+
+const viewAllRoles = async () => {
+  connection.query(
+    "SELECT employee_tracker.roles.id, employee_tracker.roles.job_title, employee_tracker.roles.salary, employee_tracker.department.name AS department FROM employee_tracker.roles JOIN employee_tracker.department ON employee_tracker.roles.department_id = employee_tracker.department.id",
+    async (error, response) => {
+      if (error) {
+        console.log("An Error Exists!");
+        console.log(error.message);
+      }
+      console.log(table.print(response));
+      await main();
+    }
+  );
+};
+
