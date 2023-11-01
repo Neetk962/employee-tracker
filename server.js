2f78e11848e2c7114b1e868bd3d113fab283e5cc
Ravneet Kaur <ravneet.kaur9629@gmail.com>
2023-10-31 20:01:53 -0700

    set up answers

--- server.js
+++ server.js
@@ -29,6 +29,30 @@ const main = async () => {
             "update an employee role",
             "exit app"
         ]
+    },
+
+]).then(async (answer) => {
+    if (answer.menu === "view all departments") {
+        viewAllDepartments();
+    } else if (answer.menu === "view all roles"){
+        viewAllRoles();
+    }else if (answer.menu === "view all employees"){
+        viewAllEmployees();
+    }else if (answer.menu === "add a department"){
+        addDepartment();
     }
-]);
+    else if (answer.menu === "add a role"){
+        AddRole();
+    }
+    else if (answer.menu === "add an employee"){
+        AddEmployee();
+    }   
+    else if (answer.menu === "update an employee role"){
+        UpdateEmployee();
+    }   
+    else if (answer.menu === "exit app"){
+      console.log("goodbye!");
+    connection.end();
+    }   
+});
 };
